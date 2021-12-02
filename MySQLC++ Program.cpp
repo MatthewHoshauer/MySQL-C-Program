1cbba784607fd4c94dbb0a254b0b933859434f37
Matthew Hoshauer <70220930+MatthewHoshauer@users.noreply.github.com>
2021-12-02 14:09:48 -0500

    Add files via upload

---
+++ MySQLC++ Program.cpp
@@ -0,0 +1,25 @@
+
+
+/* Standard C++ includes */
+#include <stdlib.h>
+#include <iostream>
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace std;
+
+int main()
+{
+
+// Creating a connection
+driver = get_driver_instance();
+con = driver->connect
+
+
+return EXIT_SUCCESS;
+*
+}
